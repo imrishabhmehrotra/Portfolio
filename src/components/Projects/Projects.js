763a56a173b1029daaec92539a0084188f0bc7e7
Rishabh Mehrotra <rishabh.2024en1164@kiet.edu>
2023-12-12 21:26:10 +0530

    Fixed Typos

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -23,21 +23,22 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={HandTracker}
-              isBlog={false}
-              title="Hand Tracker"
-              description="The MediaPipe Hands module provides a pre-trained model for hand tracking, which can detect and track the landmarks (key points) of the human hand in images or video frames. Each landmark corresponds to a specific point on the hand, such as fingertips, knuckles, and palm center.  "
-              ghLink="https://github.com/imrishabhmehrotra/SELF_DRIVING_CAR.git"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
               imgPath={chatify}
               isBlog={false}
               title="ChatShat"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/imrishabhmehrotra/CHATSHAT.git"
+              demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={HandTracker}
+              isBlog={false}
+              title="Hand Tracker"
+              description="The MediaPipe Hands module provides a pre-trained model for hand tracking, which can detect and track the landmarks (key points) of the human hand in images or video frames. Each landmark corresponds to a specific point on the hand, such as fingertips, knuckles, and palm center.  "
+              ghLink="https://github.com/imrishabhmehrotra/SELF_DRIVING_CAR.git"
               demoLink="https://chatify-49.web.app/"
             />
           </Col>
@@ -57,7 +58,7 @@ function Projects() {
               imgPath={emotion}
               isBlog={false}
               title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backend. The classifier successfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
               ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
             />
@@ -81,7 +82,7 @@ function Projects() {
               This project aims to showcase the principles of autonomous vehicle control and navigation through a simple and interactive web-based simulation.
               "
               ghLink="https://github.com/imrishabhmehrotra/SELF_DRIVING_CAR.git"
-              demoLink="https://chatify-49.web.app/"
+              demoLink="https://selfdrivingcar-mocha.vercel.app/"
             />
           </Col>
         </Row>
